c540ca49807f7ea9f3e73eb193baa1273d843b68
Oriol Monte <oriol.monte95@gmail.com>
2024-02-06 19:36:16 +0100

    Primeros Pasos Menu

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -6,5 +6,30 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        static String[] GetTemas (string fileName)
+        {
+            List<String> list = new List<String>();
+            StreamReader sr = new StreamReader(fileName);
+            string cursor = sr.ReadLine();
+            while (cursor != null) 
+            {
+                list.Add(cursor);               
+            }
+            return list.ToArray();
+        }
+        static string GetOption (int opcion, String[] temas)
+        {
+            return temas[opcion];
+        }
+        static void ShowOptions(String[] temas)
+        {         
+            int count = 1;
+            foreach (String t in temas) 
+            {
+                Console.WriteLine($"{count} - {t}");
+            }
+            Console.WriteLine("E - Exit");
+        }
+
     }
 }
