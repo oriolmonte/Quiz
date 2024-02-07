aada75c7adec1493893a633464aeb2056c45be0c
Oriol Monte <oriol.monte95@gmail.com>
2024-02-07 20:56:42 +0100

    Get Pregunta v1

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -4,49 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string pregunta = "10";
+            GetPregunta( pregunta );
         }
         public static void GetPregunta(string pregunta) 
         {
             Console.Clear();
-            bool trobat = false;
             Random r = new Random();
             StreamReader sr = new StreamReader($"{pregunta}.txt");
             int maxPreguntas = Convert.ToInt32(sr.ReadLine());
-            int preguntaElegida = r.Next(1, maxPreguntas);
+            int preguntaElegida = r.Next(0, maxPreguntas);
             int respuestaCorrecta;
             string cursor = sr.ReadLine();
-            while (cursor != null && !trobat) 
+            for(int i = 0; i<preguntaElegida*5; i++)
             {
-                if (Convert.ToInt32(cursor) == preguntaElegida)
-                {
-                    trobat = true;
-                }
-                else
-                    cursor = sr.ReadLine();
+                cursor = sr.ReadLine();
             }
-            if (!trobat) throw new Exception("An unexpected error has ocurred");
+            Console.Clear();
+            Console.WriteLine(cursor);
+            cursor = sr.ReadLine();
+            respuestaCorrecta = Convert.ToInt32(cursor);
+            for(int i = 0; i < 3; i++)
+            {
+                cursor = sr.ReadLine();
+                Console.WriteLine(cursor);
+            }
+            Console.Write("Write the correct answer number:");
+            int respuesta = Convert.ToInt32(Console.ReadLine());
+            Console.Clear() ;
+            if (respuestaCorrecta == respuesta)
+            {
+               Console.WriteLine("YOU WON!!!!!!!!");
+            }
             else
             {
-                respuestaCorrecta= Convert.ToInt32(cursor);
-                for(int i = 0; i < 2; i++)
-                {
-                    cursor = sr.ReadLine();
-                    Console.WriteLine(cursor);
-                }
-                Console.Write("Write the correct answer number:");
-                int respuesta = Convert.ToInt32(Console.ReadLine());
-                Console.Clear() ;
-                if (respuestaCorrecta == respuesta)
-                {
-                    MsgNextScreen("YOU WON!!!!!!!!");
-                }
-                else
-                {
-                    MsgNextScreen("Try again...");
-                }
-                 
+                Console.WriteLine("Try again...");
             }
+             
 
         
         }
